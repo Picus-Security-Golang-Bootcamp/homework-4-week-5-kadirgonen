abf026a7cecd6c5a11287e1efa5600b888997e41
Kadir GÖNEN <83732892+kadirgonen@users.noreply.github.com>
2022-04-02 02:28:14 +0300

    Add to knowledge of BuyBookByID function
    Adding knowledge of BuyBookByID function

--- api/app/handler/index_handler.go
+++ api/app/handler/index_handler.go
@@ -13,6 +13,7 @@ func IndexRouting() *mux.Router {
 	r.HandleFunc("/books/{id}", UpdateBook).Methods("PUT")
 	r.HandleFunc("/books/{id}", DeleteBook).Methods("DELETE")
 	r.HandleFunc("/bookname/{name}", GetSearchBook).Methods("GET")
+	r.HandleFunc("/books/{id}/{number}", BuyBookByID).Methods("GET")
 	//? <--------------------Authors-------------------------->
 	r.HandleFunc("/authors", GetAllAuthors).Methods("GET")
 	r.HandleFunc("/authors/{id}", GetAuthor).Methods("GET")
